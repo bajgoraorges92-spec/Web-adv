6857ebb82c8cbc1b1625d8e0e1484c3adcbe3ff9
bajgoraorges92-spec <bajgoraorges92@gmail.com>
2026-03-23 11:48:20 +0100

    javascript

---
+++ Lesson 26/main.js
@@ -0,0 +1,26 @@
+var num1 = 14
+var num2 = 7
+var shuma = num1 + num2
+var minus = num1 - num2
+var shumzim = num1 * num2
+var pjestim = num1 / num2
+console.log(shuma)
+console.log(minus)
+console.log(shumzim)
+console.log(pjestim)
+
+var name = "orges"
+console.log(name)
+
+var orges_r = 20
+var orges_b = 18
+var mosha = orges_r + orges_b
+
+var gentrit = 14
+var oltion = 21
+var mosha1 = gentrit + oltion
+
+console.log(mosha==mosha1)
+console.log(mosha!=mosha1)
+console.log(mosha>=mosha1)
+console.log(mosha<=mosha1)
