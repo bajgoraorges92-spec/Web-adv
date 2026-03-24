51a561dbc20e43b54b90d6b5692443e9bc5167cd
bajgoraorges92-spec <bajgoraorges92@gmail.com>
2026-03-24 11:46:07 +0100

    java

--- Lesson 26/main.js
+++ Lesson 26/main.js
@@ -1,26 +1,37 @@
-var num1 = 14
-var num2 = 7
-var shuma = num1 + num2
-var minus = num1 - num2
-var shumzim = num1 * num2
-var pjestim = num1 / num2
-console.log(shuma)
-console.log(minus)
-console.log(shumzim)
-console.log(pjestim)
+var num1 = 14;
+var num2 = 7;
+var shuma = num1 + num2;
+var minus = num1 - num2;
+var shumzim = num1 * num2;
+var pjestim = num1 / num2;
+console.log(shuma);
+console.log(minus);
+console.log(shumzim);
+console.log(pjestim);
 
-var name = "orges"
-console.log(name)
+var name = "orges";
+console.log(name);
 
-var orges_r = 20
-var orges_b = 18
-var mosha = orges_r + orges_b
+var orges_r = 20;
+var orges_b = 18;
+var mosha = orges_r + orges_b;
 
-var gentrit = 14
-var oltion = 21
-var mosha1 = gentrit + oltion
+var gentrit = 14;
+var oltion = 21;
+var mosha1 = gentrit + oltion;
 
-console.log(mosha==mosha1)
-console.log(mosha!=mosha1)
-console.log(mosha>=mosha1)
-console.log(mosha<=mosha1)
+console.log(mosha==mosha1);
+console.log(mosha!=mosha1);
+console.log(mosha>=mosha1);
+console.log(mosha<=mosha1);
+
+var age = 40;
+console.log(age>=18 && age<=65);
+
+var age1 = 20;
+var age2 = 22;
+console.log(age1==20 || age2==23);
+
+var var1 = 13
+var var2 = 14
+console.log(!(var1==var2));
---
+++ lesson 27/main.js
@@ -0,0 +1,11 @@
+function shuma (num1,num2){
+    console.log(num1+num2)
+}
+
+shuma (14, 20)
+
+function zbritja (num1, num2){
+    console.log(num1-num2)
+}
+
+zbritja (35, 22)
