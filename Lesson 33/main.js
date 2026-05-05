15878b551d750383bd6dd0e46b4cdb17c3dc5678
Orgesi <bajgoraorges92@gmail.com>
2026-05-05 11:48:10 +0200

    Lesson 33

---
+++ Lesson 33/main.js
@@ -0,0 +1,10 @@
+function validation(){
+
+    var name = document.getElementById("name").value;
+    valid_name_regex = /^[A-Za-z]+$/;
+
+    var age = document.getElementById("age").valid;
+    valid_age_regex = /^[0-9]+$/;
+
+    var city = document.getElementById("city").valid;
+}
